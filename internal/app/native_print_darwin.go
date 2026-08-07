f88528147d2c20acc33e257fc740591680da6617
DINKIssTyle <162435433+DINKIssTyle@users.noreply.github.com>
2026-08-07 17:19:37 +0900

    fix(macOS): enable Paper Size and Orientation (Portrait/Landscape) options in native print dialog

--- internal/app/native_print_darwin.go
+++ internal/app/native_print_darwin.go
@@ -4,9 +4,22 @@ package app
 
 /*
 #cgo CFLAGS: -x objective-c
-#cgo LDFLAGS: -framework Foundation -framework Cocoa
+#cgo LDFLAGS: -framework Foundation -framework Cocoa -framework WebKit
 #import <Foundation/Foundation.h>
 #import <Cocoa/Cocoa.h>
+#import <WebKit/WebKit.h>
+
+static WKWebView* DKSTFindWKWebView(NSView *view) {
+    if (view == nil) return nil;
+    if ([view isKindOfClass:[WKWebView class]]) {
+        return (WKWebView*)view;
+    }
+    for (NSView *subview in view.subviews) {
+        WKWebView *found = DKSTFindWKWebView(subview);
+        if (found != nil) return found;
+    }
+    return nil;
+}
 
 static void DKSTShowPageSetup(void *nativeWindow) {
     dispatch_async(dispatch_get_main_queue(), ^{
@@ -17,6 +30,32 @@ static void DKSTShowPageSetup(void *nativeWindow) {
                                                  delegate:nil
                                            didEndSelector:nil
                                               contextInfo:nil];
+    });
+}
+
+static void DKSTPrintCurrentWindow(void *nativeWindow) {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        NSWindow *window = (__bridge NSWindow*) nativeWindow;
+        if (window == nil) return;
+        WKWebView *webView = DKSTFindWKWebView(window.contentView);
+        if (webView != nil) {
+            NSPrintInfo *printInfo = [NSPrintInfo sharedPrintInfo];
+            NSPrintOperation *printOp = [webView printOperationWithPrintInfo:printInfo];
+            if (printOp != nil) {
+                NSPrintPanel *printPanel = [printOp printPanel];
+                if (printPanel != nil) {
+                    NSPrintPanelOptions options = [printPanel options];
+                    options |= NSPrintPanelShowsPaperSize | NSPrintPanelShowsOrientation | NSPrintPanelShowsPageSetupAccessory;
+                    [printPanel setOptions:options];
+                }
+                [printOp setShowsPrintPanel:YES];
+                [(NSPrintOperation*)printOp runOperationModalForWindow:window
+                                                              delegate:nil
+                                                        didEndSelector:NULL
+                                                           contextInfo:NULL];
+                return;
+            }
+        }
     });
 }
 */
@@ -33,7 +72,9 @@ func showPageSetup(window *application.WebviewWindow) {
 
 //wails:ignore
 func printCurrentWindow(window *application.WebviewWindow) {
-	if window != nil {
+	if window != nil && window.NativeWindow() != nil {
+		C.DKSTPrintCurrentWindow(window.NativeWindow())
+	} else if window != nil {
 		_ = window.Print()
 	}
 }
